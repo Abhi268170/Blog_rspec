ab285e67dba4d46cca0adf978f48606d8ee1e3b3
Abhi <Abhijith.sa@pitsolutions.com>
2022-07-25 10:23:44 +0530

    till comments and basic views

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
+  let(:user) { create(:user) }
+  it "an example" do
+    sign_in user
+    get "/articles"
+    expect(response).to have_http_status(:ok)
+    expect(response).to render_template(:index)
+  end
     
   end
 end
